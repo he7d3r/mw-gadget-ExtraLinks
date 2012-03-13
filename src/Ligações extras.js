2cf6505a29f73cc2d7da548a30ee4f7e53c20fe7
Helder <he7d3r@gmail.com>
2012-03-13 20:11:48 +0000

    Remove code moved to other script
    For the replacement, see he7d3r/mw-gadget-LinksOnJSPages.

--- src/Ligações extras.js
+++ src/Ligações extras.js
@@ -3,7 +3,7 @@
 $(function () {
 'use strict';
 
-var link, $link, encodedBookName, user, proj, code, d, mes, ano, path, catNS, regexes;
+var link, $link, encodedBookName, user, proj, code, d, mes, ano;
 
 if ( $.inArray( mw.config.get( 'wgDBname' ), [ 'ptwikibooks', 'wikilocaldb' ] ) !== -1 && mw.config.get( 'wgNamespaceNumber' ) === 0 ) {
 	encodedBookName = mw.util.wikiUrlencode( mw.config.get( 'wgBookName' ) );
@@ -179,44 +179,5 @@ $(function () {
 		$(this).attr('href', newHref);
 	});
 });
-
-//Workaround for [[bugzilla:10410]]
-//Convert link syntax [[zz]] to true links on javascript and css pages
-path = mw.config.get('wgArticlePath');
-catNS = mw.config.get('wgFormattedNamespaces')['14'];
-regexes = [
-	[ // [[Links like this]]
-		/\[\[\s*([^\|\]]+?)\s*\]\]/ig,
-		'[[<a href="' + path + '">$1</a>]]'
-	],
-	[ // [[Links like this|with an alternative text]]
-		/\[\[\s*([^\|\]]+?)\s*\|\s*([^\]]+?)\s*\]\]/ig,
-		'[[<a href="' + path + '">$2</a>]]'
-	],
-	[ // [[Category:Links|with an index for sorting]]
-		new RegExp('\\[\\[<a href="' + path.replace('$1', '(?:Category|' + catNS + '):([^"]+)') + '">([^<]+)</a>\\]\\]', 'gi'),
-		'[[<a href="' + path.replace('$1', 'Category:$1') + '">' + catNS + ':$1</a>|$2]]'
-	],
-	[ // Links to MediaWiki site (Workaround for [[bugzilla:22407]])
-		/href="\/wiki\/mw:/ig,
-		'href="' + '//www.mediawiki.org/wiki/'
-	]
-];
-
-function createLinks(t) {
-	var i;
-	for (i=0; i< regexes.length; i++) {
-		t = t.replace(regexes[i][0], regexes[i][1]);
-	}
-	return t;
-}
-
-if ($.inArray(mw.config.get('wgNamespaceNumber'), [2, 8]) !== -1 && mw.config.get('wgPageName').match(/\.(js|css)$/) && $.inArray(mw.config.get('wgAction'), ['view', 'purge']) !== -1) {
-	$('#bodyContent pre')
-		.first().find('span.coMULTI, span.co1') //FIXME: "span.st0" makes this too slow =(
-		.each(function () {
-			$(this).html(createLinks($(this).html()));
-		});
-}
 
 });
