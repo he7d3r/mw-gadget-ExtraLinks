5c6c6fd8b6bde24d25af3192d4770048584fe128
Helder <he7d3r@gmail.com>
2013-08-07 12:38:24 +0000

    Provide links to Special:PermaLink, update link for stats and validate with JSHint

--- src/Ligações extras.js
+++ src/Ligações extras.js
@@ -3,21 +3,32 @@
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Ligações extras.js]] ([[File:User:Helder.wiki/Tools/Ligações extras.js]])
  */
-/*jslint browser: true, white: true, regexp: true, todo: true */
-/*global mediaWiki, jQuery */
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, laxbreak: true, devel: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
 ( function ( mw, $ ) {
 'use strict';
 
-var link, $link, encodedBookName, user, proj, code, d, mes, ano;
+var link, $link, encodedBookName, user, proj, code, specialPermaLink;
 
-if ( $.inArray( mw.config.get( 'wgDBname' ), [ 'ptwikibooks', 'wikilocaldb' ] ) !== -1 && mw.config.get( 'wgNamespaceNumber' ) === 0 ) {
+if ( $.inArray( mw.config.get( 'wgDBname' ), [ 'ptwikibooks', 'my_wiki' ] ) !== -1
+	&& mw.config.get( 'wgNamespaceNumber' ) === 0
+) {
 	encodedBookName = mw.util.wikiUrlencode( mw.config.get( 'wgBookName' ) );
 	link = '//toolserver.org/~pathoschild/catanalysis/?title='
 		+ encodedBookName + '&cat=0&wiki=ptwikibooks_p';
-	mw.util.addPortletLink( 'p-tb', link, 'Estatísticas do livro', 't-catanalysis', 'Ver estatísticas sobre este livro', 't', '#t-whatlinkshere');
+	mw.util.addPortletLink(
+		'p-tb',
+		link,
+		'Estatísticas do livro',
+		't-catanalysis',
+		'Ver estatísticas sobre este livro',
+		't',
+		'#t-whatlinkshere'
+	);
 	link = mw.util.wikiGetlink( 'Special:RecentChangesLinked' )
 		+ '?days=30&limit=500&target=Categoria:Livro/' + encodedBookName;
-	mw.util.addPortletLink('p-navigation',
+	mw.util.addPortletLink(
+		'p-navigation',
 		link,
 		'Mudanças neste livro',
 		'ca-bookrecentchanges',
@@ -30,7 +41,8 @@ if ( $.inArray( mw.config.get( 'wgDBname' ), [ 'ptwikibooks', 'wikilocaldb' ] ) 
 if ( 0 <= mw.config.get( 'wgNamespaceNumber' ) ) {
 	link = mw.util.wikiGetlink( 'Special:RecentChangesLinked/' + mw.config.get('wgPageName') )
 		+ '?namespace=0&showlinkedto=1&days=30&limit=500';
-	mw.util.addPortletLink('p-navigation',
+	mw.util.addPortletLink(
+		'p-navigation',
 		link,
 		'Mudanças relacionadas',
 		'ca-recentchangeslinked',
@@ -42,13 +54,22 @@ if ( 0 <= mw.config.get( 'wgNamespaceNumber' ) ) {
 
 // Adiciona uma ligação para as contribuições globais de um usuário
 if ( $.inArray( mw.config.get( 'wgNamespaceNumber' ), [ 2, 3 ] ) !== -1
-	|| 'Contributions' === mw.config.get('wgCanonicalSpecialPageName') ){
+	|| 'Contributions' === mw.config.get('wgCanonicalSpecialPageName')
+){
 	if ( mw.config.get('wgNamespaceNumber') === -1 ){
-			user = $('input[name="target"]').val();
+		user = $('input[name="target"]').val();
 	} else {
-			user = mw.config.get( 'wgTitle' ).split( '/' )[0];
+		user = mw.config.get( 'wgTitle' ).split( '/' )[0];
 	}
-	mw.util.addPortletLink( 'p-tb', '//toolserver.org/~luxo/contributions/contributions.php?user=' + mw.util.wikiUrlencode( user ), 'Contribuições globais', 't-global', 'Ver as contribuições globais de ' + user, 'g', '#t-contributions + li' );
+	mw.util.addPortletLink(
+		'p-tb',
+		'//toolserver.org/~luxo/contributions/contributions.php?user=' + mw.util.wikiUrlencode( user ),
+		'Contribuições globais',
+		't-global',
+		'Ver as contribuições globais de ' + user,
+		'g',
+		'#t-contributions + li'
+	);
 
 	// Rename default link
 	$( '#t-contributions' ).find( 'a' ).text( 'Contribuições');
@@ -56,7 +77,7 @@ if ( $.inArray( mw.config.get( 'wgNamespaceNumber' ), [ 2, 3 ] ) !== -1
 
 
 //Adiciona ao topo das mensagens de sistema uma aba com ligação para o Translatewiki
-if (8 === mw.config.get( 'wgNamespaceNumber' ) ) {
+if ( 8 === mw.config.get( 'wgNamespaceNumber' ) ) {
 	mw.util.addPortletLink(
 		'p-namespaces',
 		'//translatewiki.net/wiki/' + mw.util.wikiUrlencode( mw.config.get( 'wgPageName' ) ) +
@@ -79,15 +100,10 @@ code = {
 	'wikiversity': '.v'
 };
 
-link = 'http://stats.grok.se/' + mw.config.get( 'wgContentLanguage' ) + code[proj] + '/';
-d=new Date();
-mes = d.getMonth()+1;
-mes = mes<10? '0'.concat(mes): mes;
-ano = d.getFullYear();
-link = link.concat(ano).concat(mes) + '/' + mw.config.get( 'wgPageName' );
 mw.util.addPortletLink(
 	'p-tb',
-	link,
+	'http://stats.grok.se/' + mw.config.get( 'wgContentLanguage' ) + code[proj] +
+		'/latest90/' + mw.config.get( 'wgPageName' ),
 	'Exibições da página',
 	't-stat',
 	'Ver estatísticas sobre a visualização desta página',
@@ -98,18 +114,29 @@ mw.util.addPortletLink(
 
 
 // Subpáginas
-if (document.getElementById('p-tb') && $.inArray( mw.config.get( 'wgNamespaceNumber' ), [ -1, 6] ) === -1){
+if ( document.getElementById('p-tb') && $.inArray( mw.config.get( 'wgNamespaceNumber' ), [ -1, 6 ] ) === -1 ){
 	link = mw.util.wikiGetlink( 'Special:PrefixIndex/' + mw.config.get( 'wgPageName' ) );
-	mw.util.addPortletLink('p-tb', link, 'Subpáginas', 't-subpages', 'Subpáginas desta página');
+	mw.util.addPortletLink( 'p-tb', link, 'Subpáginas', 't-subpages', 'Subpáginas desta página' );
 }
 
 // Add a short permanent link (without 'title=...')
-$link = $( '#t-permalink' ).find( 'a' ).text('Link perm.');
-if ( $link.size() ) {
-	$link.after( $( '<a>' )
-		.attr( 'href', $link.attr( 'href' ).replace( /title=[^&]*&/, '' ) )
-		.text( 'Link curto' )
-	).after( ' / ' );
-}
+$link = $( '#t-permalink' ).find( 'a' ).text( 'Link perm.' );
+specialPermaLink = 'Special:PermaLink/' + mw.util.getParamValue( 'oldid', $link.attr( 'href' ) );
+$link.after(
+	' / ',
+	$( '<a>' )
+		.attr(
+			'href',
+			mw.util.wikiGetlink( specialPermaLink )
+		)
+		.text( '[[wiki]]' )
+		.click( function( e ){
+			e.preventDefault();
+			prompt(
+				'Ligação interna:',
+				'[[' + specialPermaLink + ']]'
+			);
+		} )
+);
 
 }( mediaWiki, jQuery ) );
